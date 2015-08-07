b9201dbe8e561ef6fa54f70ac60c3374a8893541
Evawere Ogbe <eoogbe@stanford.edu>
2015-08-06 18:20:23 -0600

    Migration weirdness

--- db/migrate/20150625192333_add_name_to_users.rb
+++ db/migrate/20150625192333_add_name_to_users.rb
@@ -1,7 +1,5 @@
 class AddNameToUsers < ActiveRecord::Migration
   def change
-    remove_index :users, :username
-    remove_column :users, :username, :string
     add_column :users, :name, :string
   end
 end
