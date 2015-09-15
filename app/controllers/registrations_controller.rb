bc47388d2b76dc0973a16428118b512398dea915
Evawere Ogbe <eoogbe@stanford.edu>
2015-09-15 13:18:47 -0600

    Added survey redirection to registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,13 +9,15 @@ class RegistrationsController < Devise::RegistrationsController
     resource.role = params[:program]
     resource.role = :control if resource.undergrad? && rand >= 0.5
 
-    if resource.save
+    if params[:school] && resource.save
       set_flash_message :notice, :signed_up if is_flashing_format?
       sign_up(resource_name, resource)
-      respond_with resource, location: after_sign_up_path_for(resource)
+      location = ENV["#{params[:school]}_#{params[:program]}_SURVEY_PATH".upcase]
+      respond_with resource, location: location
     else
       clean_up_passwords resource
       set_minimum_password_length
+      resource.errors[:base] << "School can't be blank" if params[:school].nil?
       respond_with resource
     end
   end
